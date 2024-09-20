cbc123eecb8d28a92096c960a771c9517ac6539e
DIEGO-ORE-ENCISO <encisodiego155@gmail.com>
2024-09-20 16:06:23 -0500

    Primer Commit

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+<script>
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay:400
+});
+ScrollReveal().reveal('.barra', { delay: 500 , origin: "right"});
+ScrollReveal().reveal('.barra', { delay: 500 , origin: "right"});
+ScrollReveal().reveal('.header__texto', { delay: 500 , origin: "bottom"});
+</script>
